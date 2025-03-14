ab7462ffe6c35ed095a706d587d9b8059014cfa7
Edy Beingolea <94944421+EdyBeingolea@users.noreply.github.com>
2025-03-14 20:40:13 +0000

    correcion

--- proyecto/src/main/java/pe/edu/vallegrande/proyecto/config/CorsConfig.java
+++ proyecto/src/main/java/pe/edu/vallegrande/proyecto/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer (){
         return new WebMvcConfigurer() {
          @Override
-         public void addCorsMappings(CorsRegistry registry){
+         public void addCorsMappings(@SuppressWarnings("null")  CorsRegistry registry){
              registry.addMapping("/**")
              .allowedOrigins("https://supreme-eureka-67j59vj949jh7g7-8086.app.github.dev")
              .allowedMethods("GET", "POST", "PUT", "DELETE")
--- proyecto/src/main/java/pe/edu/vallegrande/proyecto/controller/PersonaController.java
+++ proyecto/src/main/java/pe/edu/vallegrande/proyecto/controller/PersonaController.java
@@ -3,7 +3,6 @@ package pe.edu.vallegrande.proyecto.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
